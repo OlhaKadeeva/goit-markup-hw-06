6a2f1a86d56c63ea9dade22cd4e4c21b1f3b434c
Olha_Kadeeva <o.kadeeva.n@gmail.com>
2024-05-12 15:12:05 +0200

    menu for mobile version

--- js/script.js
+++ js/script.js
@@ -6,13 +6,22 @@
     closeModalBtn: document.querySelector("[data-modal-close]"),
     // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector("[data-modal]"),
+    // menu burger
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle("is-open");
+  }
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-open");
   }
 })();
